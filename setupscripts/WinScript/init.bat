d013e4be21277ea135a5a91748bb50114ddec0ae
Aparna Ullas <65327751+aparnaullas-rubix@users.noreply.github.com>
2021-06-28 16:01:47 +0300

    New Bootstrap

--- setupscripts/WinScript/init.bat
+++ setupscripts/WinScript/init.bat
@@ -2,6 +2,7 @@ ipfs init
 xcopy "%CD%\swarm.key" "C:\Users\%USERNAME%\.ipfs" /K /D /H /Y
 ipfs bootstrap rm --all
 ipfs bootstrap add /ip4/183.82.0.114/tcp/4001/ipfs/QmcjERi3TqKfLdQp4ViSPMyfGj9oxWKZRAprkppxQc2uMm
+ipfs bootstrap add /ip4/13.76.134.226/tcp/4001/ipfs/QmYthCYD5WFVm6coBsPRGvknGexpf9icBUpw28t18fBnib
 ipfs config --json Experimental.Libp2pStreamMounting true
 ipfs config --json Swarm.EnableAutoRelay true
 ipfs config --json Swarm.EnableRelayHop true
