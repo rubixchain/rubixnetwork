8ef7a9bf77392545a54c28dd1e7e31d9b0da9e15
Aparna Ullas <65327751+aparnaullas-rubix@users.noreply.github.com>
2021-06-28 16:01:06 +0300

    New Bootstrap

--- setupscripts/MacScript/init.sh
+++ setupscripts/MacScript/init.sh
@@ -3,6 +3,7 @@ IPFS_PATH=/Users/$user/.ipfs ipfs init
 cp -R swarm.key /Users/$user/.ipfs
 IPFS_PATH=/Users/$user/.ipfs ipfs bootstrap rm --all
 IPFS_PATH=/Users/$user/.ipfs ipfs bootstrap add /ip4/183.82.0.114/tcp/4001/ipfs/QmcjERi3TqKfLdQp4ViSPMyfGj9oxWKZRAprkppxQc2uMm
+IPFS_PATH=/Users/$user/.ipfs ipfs bootstrap add /ip4/13.76.134.226/tcp/4001/ipfs/QmYthCYD5WFVm6coBsPRGvknGexpf9icBUpw28t18fBnib
 IPFS_PATH=/Users/$user/.ipfs ipfs config --json Experimental.Libp2pStreamMounting true
 IPFS_PATH=/Users/$user/.ipfs ipfs config --json Swarm.EnableAutoRelay true
 IPFS_PATH=/Users/$user/.ipfs ipfs config --json Swarm.EnableRelayHop true
