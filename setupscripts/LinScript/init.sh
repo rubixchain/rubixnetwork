192691d4cece419c69dede8dc79da39d6a7385a2
Nidhin Mahesh A <nidhin.m@rubix.network>
2022-01-24 19:06:29 +0530

    updated bootstrap list in setup scripts

--- setupscripts/LinScript/init.sh
+++ setupscripts/LinScript/init.sh
@@ -2,7 +2,7 @@ user=$(whoami)
 IPFS_PATH=/home/$user/.ipfs ipfs init
 cp -R swarm.key /home/$user/.ipfs
 IPFS_PATH=/home/$user/.ipfs ipfs bootstrap rm --all 
-IPFS_PATH=/home/$user/.ipfs ipfs bootstrap add /ip4/183.82.0.114/tcp/4001/ipfs/QmcjERi3TqKfLdQp4ViSPMyfGj9oxWKZRAprkppxQc2uMm
+IPFS_PATH=/home/$user/.ipfs ipfs bootstrap add /ip4/115.124.117.37/tcp/4001/p2p/QmWXELAoKJsCMFoW3j6pFmXEhouwKgWiK7wN6uLyuX6ULV
 IPFS_PATH=/home/$user/.ipfs ipfs bootstrap add /ip4/13.76.134.226/tcp/4001/ipfs/QmYthCYD5WFVm6coBsPRGvknGexpf9icBUpw28t18fBnib
 IPFS_PATH=/home/$user/.ipfs ipfs config --json Experimental.Libp2pStreamMounting true
 IPFS_PATH=/home/$user/.ipfs ipfs config --json Swarm.EnableAutoRelay true
--- setupscripts/WinScript/init.bat
+++ setupscripts/WinScript/init.bat
@@ -1,7 +1,7 @@
 ipfs init
 xcopy "%CD%\swarm.key" "C:\Users\%USERNAME%\.ipfs" /K /D /H /Y
 ipfs bootstrap rm --all
-ipfs bootstrap add /ip4/183.82.0.114/tcp/4001/ipfs/QmcjERi3TqKfLdQp4ViSPMyfGj9oxWKZRAprkppxQc2uMm
+ipfs bootstrap add /ip4/115.124.117.37/tcp/4001/p2p/QmWXELAoKJsCMFoW3j6pFmXEhouwKgWiK7wN6uLyuX6ULV
 ipfs bootstrap add /ip4/13.76.134.226/tcp/4001/ipfs/QmYthCYD5WFVm6coBsPRGvknGexpf9icBUpw28t18fBnib
 ipfs config --json Experimental.Libp2pStreamMounting true
 ipfs config --json Swarm.EnableAutoRelay true
